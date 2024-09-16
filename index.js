10faf65d4066a21722c8609304d9c61081db7c77
Natheer777 <111202840+Natheer777@users.noreply.github.com>
2024-09-16 15:05:55 +0300

    u

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const { google } = require('googleapis');
 
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 // app.use(cors());
 // app.use(cors({
 //   origin: function (origin, callback) {
